a722711cf31220461361a138148dde5b64ded14d
Diana1975 <66081981+Diana1975@users.noreply.github.com>
2020-06-15 11:30:21 +0300

    Add files via upload

---
+++ NamuDarbai1/Tranzakcija.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NamuDarbai1
+{
+    public enum ValiutosTipas { EUR, SEK, LTL, GBP}
+    class Tranzakcija
+    {
+        public decimal MokejimoSuma;
+        public string MokejimoPaskirtis;
+        public DateTime Laikas = DateTime.Now;
+        public ValiutosTipas Valiuta;
+        
+
+        public void TranzakcijosInformacija()
+        {
+
+            Console.WriteLine($"{MokejimoSuma},{MokejimoPaskirtis},{Laikas},{Valiuta}");
+        }
+    }
+
+}
